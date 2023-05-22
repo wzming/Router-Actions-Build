918482c68642ee85f0c23d71ea760a98f7669ee0
King <wzmlove1121@outlook.com>
2023-05-22 15:09:18 +0800

    移除K2P固件的uhttpd和更换成argon主题

--- k2p/diy-part2.sh
+++ k2p/diy-part2.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+
+
+sed -i 's/+uhttpd +uhttpd-mod-ubus +luci-mod-admin-full +luci-theme-bootstrap/+luci-mod-admin-full/g' /workdir/openwrt/feeds/luci/collections/luci/Makefile
+sed -i 's/+luci-theme-bootstrap //g' /workdir/openwrt/feeds/luci/collections/luci-nginx/Makefile
+sed -i 's/+luci-theme-bootstrap //g' /workdir/openwrt/feeds/luci/collections/luci-ssl-nginx/Makefile
