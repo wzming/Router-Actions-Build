7ca0abb795c1bd14f7ce9fe0e225c87176704c46
King <wzmlove1121@outlook.com>
2023-06-11 18:38:57 +0800

    调整配置

---
+++ x86_64_fullcone/config.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git clone --depth 1 -b x86_64 --single-branch ${{secrets.PULL_SETTING_REPO_URL}} /workdir/config
+cd /workdir/config
+mkdir -p /workdir/openwrt/files
+mv etc /workdir/openwrt/files/
+mv root /workdir/openwrt/files/
