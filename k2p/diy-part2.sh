ad22c01c68bd33ddc0c6d65777c4c307588dc12b
king <wzmlove1121@outlook.com>
2023-07-03 23:22:48 +0800

    k2p update watchcat plus

--- k2p/diy-part2.sh
+++ k2p/diy-part2.sh
@@ -4,3 +4,10 @@
 #sed -i 's/+uhttpd +uhttpd-mod-ubus +luci-mod-admin-full +luci-theme-bootstrap/+luci-mod-admin-full/g' /workdir/openwrt/feeds/luci/collections/luci/Makefile
 #sed -i 's/+luci-theme-bootstrap //g' /workdir/openwrt/feeds/luci/collections/luci-nginx/Makefile
 #sed -i 's/+luci-theme-bootstrap //g' /workdir/openwrt/feeds/luci/collections/luci-ssl-nginx/Makefile
+
+cd /workdir/openwrt
+# 删除老版本watchcat
+rm -rf feeds/packages/utils/watchcat
+svn co https://github.com/openwrt/packages/trunk/utils/watchcat feeds/packages/utils/watchcat
+
+git clone https://github.com/gngpp/luci-app-watchcat-plus.git package/luci-app-watchcat-plus
