cce0a8c795c0886618f845592ccb06e6cdd97270
King <wzmlove1121@outlook.com>
2023-07-10 16:11:09 +0800

    update top

--- k2p/diy-part2.sh
+++ k2p/diy-part2.sh
@@ -10,4 +10,4 @@ cd /workdir/openwrt
 rm -rf feeds/packages/utils/watchcat
 svn co https://github.com/openwrt/packages/trunk/utils/watchcat feeds/packages/utils/watchcat
 
-git clone https://github.com/gngpp/luci-app-watchcat-plus.git package/luci-app-watchcat-plus
+git clone --depth 1 https://github.com/gngpp/luci-app-watchcat-plus.git package/luci-app-watchcat-plus
