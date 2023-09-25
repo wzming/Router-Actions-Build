e50c401eddbe504d0523cf6e039ab64f7177bcb9
王知明 <wzmlove1121@outlook.com>
2023-09-25 14:27:27 +0800

    remove luci-nginx for x86_64 bump kernel to 5.15.133

--- n1/diy-part1.sh
+++ n1/diy-part1.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+
+#bump kernel
+cat >> include/kernel-5.15 << EOF
+LINUX_VERSION-5.15 = .133
+LINUX_KERNEL_HASH-5.15.133 = ef845e7934897b88e4448378ea9daacac19e07f156fe904844fab0a7d8ff5ddd"
+EOF
