a1ac0df0c020f20c4cb754498d2b2218ab472b69
王知明 <wzmlove1121@outlook.com>
2023-10-20 20:01:57 +0800

    "Commented out kernel version bump and yt-dlp-src configuration
    These changes were made to temporally disable the kernel version update process in diy-part1.sh to avoid unnecessary kernel version changes for certain build tests. Moreover, the yt-dlp-src configuration in .config under x86_64_fullcone was also disabled due to possible conflicts with yt-dlp, providing a more stable build."

--- n1/diy-part1.sh
+++ n1/diy-part1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #bump kernel
-rm -rf include/kernel-5.15
-cat >> include/kernel-5.15 << EOF
-LINUX_VERSION-5.15 = .135
-LINUX_KERNEL_HASH-5.15.135 = 14835e629e237f136cfed484fdb533c95242429809bdcdb2a2d66dfcb7447170
-EOF
+#rm -rf include/kernel-5.15
+#cat >> include/kernel-5.15 << EOF
+#LINUX_VERSION-5.15 = .135
+#LINUX_KERNEL_HASH-5.15.135 = 14835e629e237f136cfed484fdb533c95242429809bdcdb2a2d66dfcb7447170
+#EOF
