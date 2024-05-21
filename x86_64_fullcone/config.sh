8f3b2e03034def478dc103c1dfec45b1cebafc20
王知明 <wzmlove1121@outlook.com>
2024-05-21 19:49:04 +0800

    自签网易云音乐证书

--- x86_64_fullcone/config.sh
+++ x86_64_fullcone/config.sh
@@ -5,6 +5,4 @@ mkdir -p /workdir/openwrt/files
 mv etc /workdir/openwrt/files/
 mv root /workdir/openwrt/files/
 #自签网易云音乐证书
-mv music-cert/ca.crt /workdir/openwrt/package/unblockneteasemusic/root/usr/share/unblockneteasemusic/core
-mv music-cert/server.crt /workdir/openwrt/package/unblockneteasemusic/root/usr/share/unblockneteasemusic/core
-mv music-cert/server.key /workdir/openwrt/package/unblockneteasemusic/root/usr/share/unblockneteasemusic/core
+mv music-cert/* /workdir/openwrt/package/unblockneteasemusic/root/usr/share/unblockneteasemusic/core/
