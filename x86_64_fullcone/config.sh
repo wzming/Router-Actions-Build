441c18be0e1508a96c09b4b6f278ece9a869bca4
王知明 <wzmlove1121@outlook.com>
2024-05-22 13:38:31 +0800

    自签网易云音乐证书update

--- x86_64_fullcone/config.sh
+++ x86_64_fullcone/config.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 git clone --depth 1 -b x86_64 --single-branch $1 /workdir/config
 cd /workdir/config
+tree
 mkdir -p /workdir/openwrt/files
 mv etc /workdir/openwrt/files/
 mv root /workdir/openwrt/files/
