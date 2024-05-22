f1874eee6b1a2d2e829c1f29f4e1c21eeea23fb9
王知明 <wzmlove1121@outlook.com>
2024-05-22 20:28:30 +0800

    自签网易云音乐证书update

--- x86_64_fullcone/config.sh
+++ x86_64_fullcone/config.sh
@@ -6,4 +6,4 @@ mv etc /workdir/openwrt/files/
 mv root /workdir/openwrt/files/
 #自签网易云音乐证书 下面两行代码不能同时存在，前面都已经把文件mv掉了，后面mv肯定要报错的，搞乱求了，切记切记
 #mv UnblockNeteaseMusicCert /workdir/openwrt/package/unblockneteasemusic/root/usr/share/unblockneteasemusic/core/
-mv UnblockNeteaseMusicCert /workdir/openwrt/package/unblockneteasemusic/root/usr/share/unblockneteasemusic/
+mv UnblockNeteaseMusicCert/* /workdir/openwrt/package/unblockneteasemusic/root/usr/share/unblockneteasemusic/
