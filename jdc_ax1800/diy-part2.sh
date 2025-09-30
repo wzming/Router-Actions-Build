035de15b416c331f37b8c7a1a030ae831965a437
king <wzmlove1121@gmail.com>
2025-09-30 10:57:22 +0800

    ax1800 fix rustc build

--- jdc_ax1800/diy-part2.sh
+++ jdc_ax1800/diy-part2.sh
@@ -1,1 +1,4 @@
 #!/bin/bash
+
+
+sed -i 's/--set=llvm\.download-ci-llvm=true/--set=llvm.download-ci-llvm=false/' feeds/packages/lang/rust/Makefile
--- jdc_ax1800_nowifi/diy-part2.sh
+++ jdc_ax1800_nowifi/diy-part2.sh
@@ -3,3 +3,5 @@ cd /workdir/openwrt
 #OAF
 rm -rf feeds/packages/net/open-app-filter
 git clone --depth 1 https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+
+sed -i 's/--set=llvm\.download-ci-llvm=true/--set=llvm.download-ci-llvm=false/' feeds/packages/lang/rust/Makefile
