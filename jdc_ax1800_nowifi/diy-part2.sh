3f9500134774536a103715304b70ca8b8cc1a58b
King <wzmlove1121@gmail.com>
2025-12-07 20:38:27 +0800

    删除nginx导入的无用包

--- jdc_ax1800_nowifi/diy-part2.sh
+++ jdc_ax1800_nowifi/diy-part2.sh
@@ -5,3 +5,5 @@ rm -rf feeds/packages/net/open-app-filter
 git clone --depth 1 https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 
 sed -i 's/--set=llvm\.download-ci-llvm=true/--set=llvm.download-ci-llvm=false/' feeds/packages/lang/rust/Makefile
+# 删除nginx导入的无用包
+sed -i '/+luci-app-attendedsysupgrade/d;/+luci-theme-bootstrap/d;/^$/d'  feeds/luci/collections/luci-nginx/Makefile
