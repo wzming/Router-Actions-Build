9ae4b53c4744c3d157e6725f356d5c6457f67eea
King <wzmlove1121@gmail.com>
2025-12-14 17:12:40 +0800

    移除 AX1800 nowifi 脚本中未使用的 CORE_TUN 和 CORE_DEV 相关逻辑

--- jdc_ax1800_nowifi/diy-part2.sh
+++ jdc_ax1800_nowifi/diy-part2.sh
@@ -13,8 +13,6 @@ sed -i '/+luci-app-attendedsysupgrade/d;/+luci-theme-bootstrap/d;/^$/d'  feeds/l
 
 #预置OpenClash内核和GEO数据
 export CORE_VER=https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/core_version
-export CORE_TUN=https://github.com/vernesong/OpenClash/raw/core/dev/premium/clash-linux
-export CORE_DEV=https://github.com/vernesong/OpenClash/raw/core/dev/dev/clash-linux
 export CORE_MATE=https://github.com/vernesong/OpenClash/raw/core/dev/meta/clash-linux
 
 export CORE_TYPE="arm64"
@@ -23,14 +21,8 @@ export TUN_VER=$(curl -sfL $CORE_VER | sed -n "2{s/\r$//;p;q}")
 cd feeds/luci/applications/luci-app-openclash/root/etc/openclash
 mkdir -p ./core && cd ./core
 
-curl -sfL -o ./tun.gz "$CORE_TUN"-"$CORE_TYPE"-"$TUN_VER".gz
-gzip -d ./tun.gz && mv ./tun ./clash_tun
-
 curl -sfL -o ./meta.tar.gz "$CORE_MATE"-"$CORE_TYPE".tar.gz
 tar -zxf ./meta.tar.gz && mv ./clash ./clash_meta
-
-curl -sfL -o ./dev.tar.gz "$CORE_DEV"-"$CORE_TYPE".tar.gz
-tar -zxf ./dev.tar.gz
 
 chmod +x ./clash* ; rm -rf ./*.gz
 
