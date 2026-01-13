184de972008e4d76cf79982228143a928603eea1
king <wzmlove1121@gmail.com>
2026-01-13 09:58:18 +0800

    优化 AX1800 nowifi 构建流程，精简磁盘空间管理逻辑，改进环境变量与路径处理

--- jdc_ax1800_nowifi/diy-part2.sh
+++ jdc_ax1800_nowifi/diy-part2.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-cd /workdir/openwrt
+cd $GITHUB_WORKSPACE/openwrt
 #OAF
 rm -rf feeds/packages/net/open-app-filter
 git clone --depth 1 https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
