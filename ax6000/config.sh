8edd1886ec0816b7f7f6430bb8a5ef53fa2c94fd
king <wzmlove1121@outlook.com>
2026-02-19 15:49:08 +0800

    新增AX6000编译,调整配置

--- ax6000/config.sh
+++ ax6000/config.sh
@@ -6,3 +6,4 @@ git clone --depth 1 -b ax6000 --single-branch $1 config
 cd $GITHUB_WORKSPACE/config
 mkdir -p $GITHUB_WORKSPACE/openwrt/files
 mv etc $GITHUB_WORKSPACE/openwrt/files/
+mv root $GITHUB_WORKSPACE/openwrt/files/
--- ax6000/diy-part2.sh
+++ ax6000/diy-part2.sh
@@ -7,6 +7,8 @@ cd $GITHUB_WORKSPACE/openwrt
 rm -rf feeds/packages/net/open-app-filter
 git clone --depth 1 https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 
+sed -i '/^[[:space:]]*+luci-app-attendedsysupgrade/d;/luci-app-package-manager/s/[[:space:]]*\\$//' feeds/luci/collections/luci/Makefile
+sed -i '/+luci-app-attendedsysupgrade/d;/+luci-theme-bootstrap/d;/^$/d'  feeds/luci/collections/luci-nginx/Makefile
 
 
 
